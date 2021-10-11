3bba6731bb9f7e800d5b598c31558fb2b260400c
Helogn <58104248+Helogn@users.noreply.github.com>
2021-10-11 21:36:27 +0800

    2021-10-11
    assignment

--- test.py
+++ test.py
@@ -7,9 +7,67 @@
 
 
 # 2021-10-11
-Fighter = ["F22","F31","J20","T50"]
-Cay = {"001A":"CN","NIM":"USA","Enterprise":"USA"}
-Tanker = ("T34","99A","T14")
-print(Fighter[2])
-print(Tanker[1])
-print(Cay["001A"])
+# Fighter = ["F22","F31","J20","T50"]
+# Cay = {"001A":"CN","NIM":"USA","Enterprise":"USA"}
+# Tanker = ("T34","99A","T14")
+# print(Fighter[2])
+# print(Tanker[1])
+# print(Cay["001A"])
+
+
+# def image(number,name = "He Jiang"):
+#     output_number = number * number
+#     name = "BBB"
+#     return output_number, name
+
+# b = 10
+# a = image(b)
+# print(type(a))
+# print(a[1])
+
+#keyword argument:an argument preceded by an identifier////different from positional
+#
+#keyword
+#
+
+# def function (name,*argument,**keyword,):
+#     print("The tanker's name is "+ name)
+#     for arg in argument:
+
+#         print("this position will output argument: "+ arg)
+
+#     print("\n")
+
+#     for arg1 in keyword:
+#         print("this position will output keyword "+ arg1)
+
+# a = function("T34","RUSSIA","1941-6-22",tanker = "wow!")
+
+#scope and Namespace： Namespaces are given directly
+
+
+#---------------------namespace---------------------
+#
+# def name_scale():
+#     g = 3
+#     def hhh():
+#         nonlocal g
+#         g = 12
+#         print(g)
+#     global hh
+#     hhh()
+
+# name_scale()
+
+
+#-------------------Class
+
+class Tanker:
+    def __init__(self = "sss"):
+        self.a = "T34"
+        self.b = "T90"
+
+a = Tanker()
+print(a.a)
+print(a)
+
